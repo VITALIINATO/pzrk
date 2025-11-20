0958c8e1286b5647751f8ec0e8f8310ed70364be
VITALIINATO <115832818+VITALIINATO@users.noreply.github.com>
2025-11-20 19:08:28 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const GOOGLE_SCRIPT_URL = "https://script.google.com/u/0/home/projects/1mmwETaH6
 
 // 2. Вставьте ссылку на публикацию таблицы для отображения в админке
 // (Файл -> Поделиться -> Опубликовать в интернете -> Встроить -> скопируйте ссылку из src="...")
-const GOOGLE_SHEET_EMBED_URL = "https://docs.google.com/spreadsheets/d/e/AKfycbzlp0tlxmJbaMmwnMkE4_-QB3qdvaJ_zUji-xp0vqhNpLencdJbuehhpLz4de6ASTJS/pubhtml?widget=true&amp;headers=false";
+const GOOGLE_SHEET_EMBED_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-0C7Jn3eccb1L9b4NtNK1sVSY2C6XATxErumLP_UsUtYZfKpp0Saw1-5uKMAiJlWk8qrUAP3cRgaI/pubhtml?widget=true&amp;headers=false";
 
 // ПАРОЛИ
 const USER_PASSWORD = "123";   // Пароль для прохождения теста
