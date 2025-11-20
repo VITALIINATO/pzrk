804abf33425f8028f52991f7b678ada18e07af6e
VITALIINATO <115832818+VITALIINATO@users.noreply.github.com>
2025-11-20 18:48:31 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+// 1. Вставьте URL веб-приложения из Google Apps Script (заканчивается на /exec)
+const GOOGLE_SCRIPT_URL = "https://script.google.com/u/0/home/projects/1mmwETaH61I8Vwi43MOQbg6iF4xGq8plPoKdfWRMwxdsO4dlSVbiJid7n/exec";
+
+// 2. Вставьте ссылку на публикацию таблицы для отображения в админке
+// (Файл -> Поделиться -> Опубликовать в интернете -> Встроить -> скопируйте ссылку из src="...")
+const GOOGLE_SHEET_EMBED_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-0C7Jn3eccb1L9b4NtNK1sVSY2C6XATxErumLP_UsUtYZfKpp0Saw1-5uKMAiJlWk8qrUAP3cRgaI/pubhtml?widget=true&amp;headers=false";
+
+// ПАРОЛИ
+const USER_PASSWORD = "NATO";   // Пароль для прохождения теста
+const ADMIN_PASSWORD = "NATO999";  // Пароль для входа в админку
