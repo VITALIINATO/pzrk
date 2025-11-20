52168b079df7035c6ae19d3d64dcb91d1d6120d1
VITALIINATO <115832818+VITALIINATO@users.noreply.github.com>
2025-11-20 19:47:31 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 
 // 1. Вставьте URL веб-приложения из Google Apps Script (заканчивается на /exec)
+// ВАЖНО: При развертывании выберите "Who has access: Anyone" (У кого есть доступ: Все)
 const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzlp0tlxmJbaMmwnMkE4_-QB3qdvaJ_zUji-xp0vqhNpLencdJbuehhpLz4de6ASTJS/exec";
 
-// 2. Вставьте ссылку на публикацию таблицы для отображения в админке
-// (Файл -> Поделиться -> Опубликовать в интернете -> Встроить -> скопируйте ссылку из src="...")
-const GOOGLE_SHEET_EMBED_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-0C7Jn3eccb1L9b4NtNK1sVSY2C6XATxErumLP_UsUtYZfKpp0Saw1-5uKMAiJlWk8qrUAP3cRgaI/pubhtml";
+// 2. Вставьте ссылку для отображения таблицы (для админки).
+// ВАРИАНТ А (Лучший): Файл -> Поделиться -> Опубликовать в интернете -> Встроить -> Скопировать ссылку из src="..."
+const GOOGLE_SHEET_EMBED_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-0C7Jn3eccb1L9b4NtNK1sVSY2C6XATxErumLP_UsUtYZfKpp0Saw1-5uKMAiJlWk8qrUAP3cRgaI/pubhtml?widget=true&headers=false";
 
 // ПАРОЛИ
-const USER_PASSWORD = "123";   // Пароль для прохождения теста
-const ADMIN_PASSWORD = "321";  // Пароль для входа в админку
+const USER_PASSWORD = "NATO";   // Пароль для прохождения теста
+const ADMIN_PASSWORD = "NATO999";  // Пароль для входа в админку
